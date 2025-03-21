9aac586c57f36b74323aad70e3010b47cd944a00
jay-laptop <jay.haynie13@gmail.com>
2025-03-21 11:52:17 -0700

    created very basic create profile and log in input fields preparing to introduce node.js next

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 	
 })
+
+const createProfileButtonDiv = document.getElementById("create-profile-div");
+const createProfileButton = document.getElementById("create-profile-button");
+const createUsernameDiv = document.getElementById("create-username-div");
+const createHeader = document.getElementById("create-header");
+const logInButtonDiv = document.getElementById("log-in-div");
+const logInButton = document.getElementById("log-in-button");
+const logInUsernameDiv = document.getElementById("log-in-username-div");
+const logHeader = document.getElementById("log-header");
+
+createProfileButton.addEventListener('click', function() {
+    createProfileButtonDiv.style.display = "none";
+    logInButtonDiv.style.display = "none";
+    createUsernameDiv.style.display = "block";
+    createHeader.style.display = "block";
+});
+
+logInButton.addEventListener('click', function() {
+    createProfileButtonDiv.style.display = "none";
+    logInButtonDiv.style.display = "none";
+    logInUsernameDiv.style.display = "block";
+    logHeader.style.display = "block";
+});
