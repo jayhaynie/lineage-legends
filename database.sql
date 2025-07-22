41dfa6e7484da288ea6b33bb7689e35b500ec579
jay-desktop <jay.haynie13@gmail.com>
2025-07-22 08:40:51 -0700

    work on card and character assets, creation, editing

---
+++ database.sql
@@ -0,0 +1,11 @@
+INSERT INTO summons (name, type, image_id, health_max, ability1_name, ability1_desc, ability1_ammount, ability1_maxuse)
+VALUES (
+	'Ethan',
+	'helper',
+	'ethan',
+	6,
+	'Obsidian Builder',
+	'Block (2) damage from the first attack each turn',
+	2,
+	0
+);
