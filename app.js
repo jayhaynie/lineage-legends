92a518ad38d5cea9ffeca6a47262d97437fb34d7
jay-desktop <jay.haynie13@gmail.com>
2025-10-03 18:03:45 -0700

    attempt 16 to deploy with database function

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // Dynamic API base URL for local and deployed environments
 const API_BASE_URL = (window.location.hostname === 'localhost' && window.location.port === '443')
     ? 'http://localhost:443'
-    : '0.0.0.0:443';// next try 10000, then try 0.0.0.0
+    : 'https://lineage-legends-postgres-db.onrender.com';// tried process.env.DATABASE_URL
 
 const titleButton = document.getElementById("title-button");
 const createHeader = document.getElementById("create-header");
